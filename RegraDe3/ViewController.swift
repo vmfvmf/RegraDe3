a5c02560f0941fe6ba0c26c0bddb3261e63a4c0c
Vinicius Martins Ferraz <vmfvmf@hotmail.com>
2016-11-02 20:14:25 -0200

    Initial Commit

---
+++ RegraDe3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RegraDe3
+//
+//  Created by Vinicius Martins Ferraz on 11/2/16.
+//  Copyright © 2016 Vinicius Martins Ferraz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RegraDe3Tests/RegraDe3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  RegraDe3Tests.swift
+//  RegraDe3Tests
+//
+//  Created by Vinicius Martins Ferraz on 11/2/16.
+//  Copyright © 2016 Vinicius Martins Ferraz. All rights reserved.
+//
+
+import XCTest
+@testable import RegraDe3
+
+class RegraDe3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
